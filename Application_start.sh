3093ce30636c666a2bbae28b7e25fceb7575f064
veerajkalkeri <111848104+veerajkalkeri@users.noreply.github.com>
2022-11-21 12:26:41 +0530

    Add files via upload

---
+++ Application_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+sudo chmod -R 777 /home/ec2-user/express-app
+#navigate into our directory where we have all our github files
+cd /home/ec2-user/express-app
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash_completion (node is in path)
+
+#install node modules
+npm install
+
+#start our node app in the background
+node server.js > server.out.log 2> server.err.log < /dev/null &
